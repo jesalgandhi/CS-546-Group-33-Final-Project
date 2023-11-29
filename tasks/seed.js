0d45202c41048d756b7d27026aabc112979c9443
jesalgandhi <jesalgandhi9988@gmail.com>
2023-11-29 15:32:14 -0500

    minor deletions to seed.js

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,7 +1,5 @@
 import { groups } from '../config/mongoCollections.js';
 import {dbConnection, closeConnection} from '../config/mongoConnection.js';
-// import attendees from '../data/attendees.js';
-// import events from '../data/events.js';
 import {groupsData, usersData, messagesData} from '../data/index.js';
 
 const db = await dbConnection();
