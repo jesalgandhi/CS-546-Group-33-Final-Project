b295683271588450b68b5b5705419a4126cafd01
Jacob Gurevich <jgurevic@stevens.edu>
2023-12-05 19:32:25 -0500

    accurate user

--- tasks/seed.js
+++ tasks/seed.js
@@ -5,9 +5,12 @@ import {groupsData, usersData, messagesData} from '../data/index.js';
 const db = await dbConnection();
 await db.dropDatabase();
 
-// groupsData.create()
-// groupsData.remove()
-// groupsData.update()
+/* issues with createUser - please fix so we can seed */
+const user1 = await usersData.createUser(
+    "John", "Smith", "johnsmith@example.com", "2015554516", "I am eager to make and meet new roommates", 23, 
+    ["Biking", "Movies", "Painting"], "picture url with be here i guess"
+);
+console.log(user1);
 
 console.log('Done seeding database');
 
