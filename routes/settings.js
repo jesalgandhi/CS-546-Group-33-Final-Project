bb357c9ce0d95d60b504241b82f4aa84a289501e
Jacob Gurevich <jgurevic@stevens.edu>
2023-12-08 16:16:29 -0500

    settings

--- routes/settings.js
+++ routes/settings.js
@@ -7,15 +7,16 @@ import {usersData} from '../data/index.js';
 import {messagesData} from '../data/index.js';
 
 
-router.route('/')
+router.route('/settings')
   .get(async (req, res) => {
-    try {
-      const userId = req.session.user._id;
-      const userSettings = await usersData.getUserById(userId);
-      res.json(userSettings);
-    } catch (e) {
-      res.status(500).json({ error: e.toString() });
-    }
+    // try {
+    //   const userId = req.session.user._id;
+    //   const userSettings = await usersData.getUserById(userId);
+    //   res.json(userSettings);
+    // } catch (e) {
+    //   res.status(500).json({ error: e.toString() });
+    // }
+    return res.render('settings', {title: 'Settings'});
   })
   // .post(async (req, res) => {
   // })
@@ -23,6 +24,23 @@ router.route('/')
     try {
       const userId = req.session.user._id;
       const updatedSettings = req.body;
+
+      if (updatedSettings.firstName && (typeof updatedSettings.firstName !== 'string' || updatedSettings.firstName.trim().length === 0)) {
+        return res.status(400).json({ error: 'Invalid first name' });
+      }
+
+      if (updatedSettings.emailAddress && !validate(updatedSettings.emailAddress)) {
+        return res.status(400).json({ error: 'Invalid email address' });
+      }
+
+      if (updatedSettings.phoneNumber) {
+        let phoneNumber = phone(updatedSettings.phoneNumber);
+        if (!phoneNumber.isValid) {
+          return res.status(400).json({ error: 'Invalid phone number' });
+        }
+        updatedSettings.phoneNumber = phoneNumber.phoneNumber;
+      }
+
       const updatedUser = await usersData.updateUser(userId, updatedSettings);
       res.json(updatedUser);
     } catch (e) {
