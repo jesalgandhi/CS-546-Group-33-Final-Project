3162b7212c590ae57967496820159c94864a43d5
Jacob Gurevich <jgurevic@stevens.edu>
2023-12-08 18:47:37 -0500

    trying to fix age(added phone number)

--- routes/register.js
+++ routes/register.js
@@ -13,14 +13,14 @@ router
     res.render("register", { title: "Register" });
   })
   .post(async (req, res) => {
-    const requiredFields = ['firstNameInput', 'lastNameInput', 'emailAddressInput', 'passwordInput', 'confirmPasswordInput', 'biographyinput', 'ageInput', 'interestsInput', 'pictureInput'];
+    const requiredFields = ['firstNameInput', 'lastNameInput', 'emailAddressInput','phonenumberInput', 'passwordInput', 'confirmPasswordInput', 'biographyInput', 'ageInput', 'interestsInput']; //'pictureInput'];
     const missingFields = requiredFields.filter(field => !req.body[field]);
 
     if (missingFields.length > 0) {
       return res.status(400).render("register", { title: "User Registration", error: missingFields.map(field => `${field.replace('Input', '')} is required`) });
     }
 
-    const { firstNameInput, lastNameInput, emailAddressInput, passwordInput, confirmPasswordInput, biographyInput, ageInput, interestsInput, pictureInput} = req.body;
+    let { firstNameInput, lastNameInput, emailAddressInput, phonenumberInput, passwordInput, confirmPasswordInput, biographyInput, ageInput, interestsInput} = req.body; //pictureInput} = req.body;
 
     const errors = [];
     if (!/^[a-zA-Z]{2,25}$/.test(firstNameInput)) errors.push("Invalid First Name");
@@ -28,16 +28,23 @@ router
     if (!/\S+@\S+\.\S+/.test(emailAddressInput.toLowerCase())) errors.push("Invalid Email Address");
     if (!/^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/.test(passwordInput)) errors.push("Invalid Password");
     if (passwordInput !== confirmPasswordInput) errors.push("Either the email address or password is invalid");
-    if (typeof biographyInput !== 'string' || biographyInput.trim().length === 0 || biographyInput.trim().length > 200) errors.push("Invalid Biography");
-    if (!Number.isInteger(parseInt(ageInput)) || ageInput < 18 || ageInput > 120) errors.push("Invalid Age");
-    if (!Array.isArray(interestsInput) || interestsInput.length === 0) errors.push("Interests are required");
-
+    if (!/^[a-zA-Z]{2,200}$/.test(biographyInput)) errors.push("Invalid Biography");
+    let age = typeof ageInput === 'number' ? ageInput : parseInt(ageInput);
+    if (!Number.isInteger(age) || age < 18 || age > 120) errors.push("Invalid Age");
+    if (typeof interestsInput === 'string') {
+      interestsInput = interestsInput.split(',').map(interest => interest.trim());
+    }
+    
+    if (!Array.isArray(interestsInput) || !interestsInput.every(interest => typeof interest === 'string')) {
+      errors.push("Interests must be a list of strings");
+    }
+    if (!/^[0-9]{10}$/.test(phonenumberInput)) errors.push("Invalid Phone Number");
     if (errors.length > 0) {
       return res.status(400).render("register", { title: "Registration Form", error: errors });
     }
 
     try {
-      const newUser = await usersData.createUser(firstNameInput, lastNameInput, emailAddressInput.toLowerCase(), passwordInput, biographyInput, ageInput, interestsInput, pictureInput);
+      const newUser = await usersData.createUser(firstNameInput, lastNameInput, emailAddressInput.toLowerCase(), phonenumberInput, passwordInput, biographyInput, ageInput, interestsInput); //pictureInput);
       if (newUser.insertedUser) {
         return res.redirect("/login");
       } else {
