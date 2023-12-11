bf0a05ff5d36b019b785f81716815d4087627686
Calvin526 <calvinmgarcia@hotmail.com>
2023-12-11 14:23:24 -0500

    Renders add group page if user GroupID is undefined (user is not in group yet)

--- routes/homepage_error.js
+++ routes/homepage_error.js
@@ -10,7 +10,15 @@ import {messagesData} from '../data/index.js';
 router
   .route('/')
   .get(async (req, res) => {
-    return res.render('homepage', {title: "Home", user: req.session.user});
+
+    console.log(req.session.user);
+    
+    if (req.session.user.groupID != undefined)
+      return res.render('homepage', {title: "Home", user: req.session.user});
+
+    else
+      return res.render('addGroup');
+
     // return res.json("homepage", {group: req.session.user.group, title: "Homepage"})
   })
   .post(async (req, res) => {
