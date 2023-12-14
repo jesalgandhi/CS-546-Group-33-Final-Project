17818d80e26022b9dc58bc2e771f50ffd4631f65
Adarsh Gogineni <adarshgogineni@gmail.com>
2023-12-13 23:21:48 -0500

    Updated Matches to include conversations
    Included Jesals conversationids in matches so now they link to conversations and creates new messages if no conversation was started

--- routes/matches.js
+++ routes/matches.js
@@ -34,17 +34,61 @@ router
   } catch (e) {
     return res.render('matches', {error: e});
   }
+  let matchesWithConversations = [];
 
   //TEMPORARY TESTING PURPOSES **********************************************
   try {
     // Get all group IDs
     matches = await groupsData.getAll();
+
+    
+    // Iterate over the matches array
+    for (let i = 0; i < matches.length; i++) {
+        let conversationId;
+        let users = [];
+
+        // Get the conversation ID for the current group ID and the matched group ID
+        try {
+          conversationId = await messagesData.getConversationIdByGroupIds(groupId, matches[i]._id.toString());
+        } catch (error) {
+          console.error(`Error getting conversation ID for match ${i}:`, error);
+        }
+
+        // Get user data for each userid in users array
+        for (let j = 0; j < matches[i].users.length; j++) {
+          try {
+            let user = await usersData.getUser(matches[i].users[j].toString());
+            users.push(user);
+          } catch (error) {
+            console.error(`Error getting user data for user ${j} in match ${i}:`, error);
+          }
+        }
+
+        // Create an object with the match and conversation ID
+        let matchWithConversation = {
+          match: matches[i],
+          conversationId: conversationId,
+          users: users,
+          latitude: matches[i].groupLocation.coordinates[1],
+          longitude: matches[i].groupLocation.coordinates[0]
+        };
+
+        // Add the object to the matchesWithConversations array
+        matchesWithConversations.push(matchWithConversation);
+  }
+  //console.log("MatchesWithCOnversationS:",matchesWithConversations);
+
+
   } catch (e) {
     return res.render('matches', {error: e});
   }
 
+  
+
+  
+
  
-  return res.render('matches', {matches});
+  return res.render('matches', {matchesWithConversations});
   // *************************************************************************
 
   //return res.render('matches', { matches });
