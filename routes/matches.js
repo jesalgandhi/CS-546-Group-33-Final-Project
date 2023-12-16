a53458ce540e63190ab849d92181e5d786f65c33
Calvin526 <calvinmgarcia@hotmail.com>
2023-12-16 16:01:53 -0500

    Commented out what I'm doing to ensure that if user clicks filtered suggestedMatch that after they click it that filtered suggestedMatches will still show rather than re-rendering homepage to show ALL suggestedMatches

--- routes/matches.js
+++ routes/matches.js
@@ -142,20 +142,71 @@ try
     let confirmedMatch = await matchesData.confirmMatch(user_id, suggested_id);
   }
 
-
-
   catch(e)
   {
     console.log(e);
   }
 
-  console.log(req.session.user.groupInfo);
+  
+  let this_city = cities.gps_lookup(req.session.user.groupInfo.groupLocation.coordinates[0], req.session.user.groupInfo.groupLocation.coordinates[1]);
+  
+  /*let suggestedMatches = [];
 
+  
+  //Gets all filtered match info WITHOUT RE-RENDERING HOMEPAGE
+  for (let x = 0; x < req.session.user.groupInfo.suggestedMatches.length; x++)
+      {
+        let thisGroup = req.session.user.groupInfo.suggestedMatches[x]
+        if (thisGroup != suggested_id)
+        {
+          try
+          {
+            let this_group = await groupsData.get(req.session.user.groupInfo.suggestedMatches[x]);
+            suggestedMatches.push(this_group);
+          }
+          catch(e)
+          {
+            console.log(e);
+          }
+        }
+      }
 
+  for (let x = 0; x < suggestedMatches.length; x++) 
+      {
+        try 
+        {
+            let groupInfo = await groupsData.get(suggestedMatches[x]._id.toString());
+            suggestedMatches[x].groupInfo = groupInfo;
+            suggestedMatches[x].this_userID = req.session.user.groupID;
+            let city = cities.gps_lookup(suggestedMatches[x].groupInfo.groupLocation.coordinates[0], suggestedMatches[x].groupInfo.groupLocation.coordinates[1]);
+            suggestedMatches[x].groupLocation.city = city;
+            console.log(city);
+        } 
+        catch (e) 
+        {
+            console.log(e);
+        }
+    
+        for (let i = 0; i < suggestedMatches[x].users.length; i++) 
+        {
+          try 
+          {
+            let this_user = await usersData.getUser(suggestedMatches[x].users[i]);
+            suggestedMatches[x].users[i] = this_user;
+          } 
+          catch (e) 
+          {
+            console.log(e);
+          }
+        }
+      }*/
+  
+  
+  //return res.render('homepage', {title: "Home", currentUser: req.session.user, user: req.session.user, group: req.session.user.groupInfo, location: this_city, groupMembers: req.session.user.groupMembers, suggestedMatches: suggestedMatches});
 
-
-
-  return res.redirect('/');
+  //return res.redirect('/');
+  
+  //return res.render()
 
 });
 
