573ec372f03cbc426585ef2df18e17da3e75b652
Jacob Gurevich <jgurevic@stevens.edu>
2023-12-15 21:27:28 -0500

    settings almost done + intersts finished

--- scripts/settingsscript.js
+++ scripts/settingsscript.js
@@ -1,41 +1,40 @@
-// settings.js
 
-document.addEventListener('DOMContentLoaded', () => {
-    const settingsForm = document.getElementById('settings-form');
-    const firstName = document.getElementById('firstNameInput');
-    const lastName = document.getElementById('lastNameInput');
-    const emailAddress = document.getElementById('emailAddressInput');
-    const phoneNumber = document.getElementById('phoneNumberInput');
-    const biography = document.getElementById('biographyInput');
-    const age = document.getElementById('ageInput');
-    const interests = document.getElementById('interestsInput');
-    const errorCheck = document.getElementById('error');
+const interests = [
+    "Music", "Movies", "Reading", "Traveling", "Hiking",
+    "Cooking", "Baking", "Fitness", "Yoga", "Meditation",
+    "Gardening", "Photography", "Art", "Crafting", "DIY Projects",
+    "Technology", "Gaming", "Sports", "Cycling", "Running",
+    "Dancing", "Environmentalism", "Volunteering", "Fashion",
+    "Writing"
+];
 
-    // Function to validate form data
-    function validateForm() {
-        if (!firstName.value.trim()) {
-            return 'First name cannot be empty.';
-        }
-        if (!lastName.value.trim()) {
-            return 'Last name cannot be empty.';
-        }
-        if (!emailAddress.value.trim()) {
-            return 'Email address cannot be empty.';
-        }
-        // Additional validations as needed...
-        return '';
-    }
+const interestsGrid = document.getElementById('interestsGrid');
 
-    if (settingsForm) {
-        settingsForm.addEventListener('submit', (event) => {
-            const errorMessage = validateForm();
-            if (errorMessage) {
-                errorCheck.hidden = false;
-                errorCheck.textContent = errorMessage;
-                event.preventDefault();
-            } else {
-                errorCheck.hidden = true;
-            }
-        });
-    }
+interests.forEach((interest) => {
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.name = 'interest';
+    checkbox.value = interest;
+    checkbox.addEventListener('change', limitSelection);
+
+    const label = document.createElement('label');
+    label.textContent = interest;
+
+    const interestDiv = document.createElement('div');
+    interestDiv.appendChild(checkbox);
+    interestDiv.appendChild(label);
+
+    interestsGrid.appendChild(interestDiv);
 });
+
+let selectedCount = 0;
+
+function limitSelection(event) {
+    const checkboxes = document.querySelectorAll('input[name="interest"]:checked');
+
+    if (checkboxes.length > 5) {
+        event.target.checked = false;
+    } else {
+        selectedCount = checkboxes.length;
+    }
+}
